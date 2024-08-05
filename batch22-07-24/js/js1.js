2f952d0a2163ff58a760e7cd0824e48b2009e077
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-08-05 15:26:28 +0500

    js start

---
+++ batch22-07-24/js/js1.js
@@ -0,0 +1,57 @@
+// hoisting
+// let  var const
+
+// var a; //  declaration
+
+// a =7 ;  // inititalization
+
+// const a=15.6789090;
+// int a =6;
+
+// dataTypes
+
+// number +> integer => float => double
+// boolean => true false
+//  function => block of code
+
+//  string => string a = " "
+// charactor => char a = 'p'
+
+// object
+//  array
+
+//  camel case
+
+// Find the area of a rectangle where the length is 5 and the width is 8.
+
+// Find the area of a triangle where the base is 4 and the height is 3.
+// Find the area of a circle where the radius is 3.
+// Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+// selector
+let div = document.getElementById("abc");
+
+let length = 5;
+let width = 8;
+
+let area = length * width;
+
+
+//  area of rectangle is  40
+ // 2 + 2 => 4 
+ // concatination / combination
+// let output = "area of rectangle is " + area;
+//  string template 
+// let output = `area of rectangle is ${area}`;
+
+div.innerHTML= output
+
+
+
+// let a = "<h1> WRITTEN BY JS 2</h1>";
+
+// div.innerHTML= a;
+
+// console.log(a);
+
+// document.write(a)
