f18011ed4441bf7290eb9af3c85ccafb7b676986
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-08-06 15:24:15 +0500

    js if and logical statements

---
+++ batch22-07-24/js/js2.js
@@ -0,0 +1,83 @@
+/**
+ * 
+  
+
+<
+>
+<= 
+
+> =
+
+
+   5 <= 5  T
+
+    F          T        T
+   5 < 5 || 5 == 5
+ 
+   comparison operator 
+
+
+   =  assignment operator  a =7;
+   ==   value compare 
+   ===  value and data type compare 
+ 
+
+
+   5 === "5"   F 
+
+   Check from the given integer, whether it is positive or negative.
+
+   */
+
+let a = -0 / 6;
+
+if (a > 0) {
+  console.log("positive number");
+} else if (a == 0) {
+  console.log("0 is Neutral number");
+} else {
+  console.log("negative number");
+}
+
+// Check two given numbers and return true
+// if one of the numbers is 50 or if their sum is 50.
+
+let q = 25;
+let t = 25;
+
+let sum = q + t;
+// (q == 50 ) || (t == 50)
+
+if (q == 50 || t == 50) {
+  console.log("one of the numbers is 50 "+true);
+} else if (sum == 50) {
+  console.log("their sum is 50  "+true);
+} else {
+  console.log(false);
+}
+
+
+//  additive identity  0
+//  multiplicative identity 1 
+
+
+// 
+
+
+// Check two given numbers and return true 
+// if one of the numbers is 50 or if their sum is 50.
+
+
+// Check from the given integer, whether it is positive or negative.
+
+//   mod return remainder 
+// Check whether a given number is even or odd.
+// Check whether a given positive number is a multiple of 3.
+
+
+//  a = -15 * -1 => 15 
+
+
+if(a < 0){
+ a = a * -1;
+}
