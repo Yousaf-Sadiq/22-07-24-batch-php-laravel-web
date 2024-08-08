8afcbded5d552f264e3286edd248f3c511621e69
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-08-08 15:36:44 +0500

    js if and logical statements

--- batch22-07-24/js/if statements/js2.js
+++ batch22-07-24/js/if statements/js2.js
@@ -49,35 +49,75 @@ let sum = q + t;
 // (q == 50 ) || (t == 50)
 
 if (q == 50 || t == 50) {
-  console.log("one of the numbers is 50 "+true);
+  console.log("one of the numbers is 50 " + true);
 } else if (sum == 50) {
-  console.log("their sum is 50  "+true);
+  console.log("their sum is 50  " + true);
 } else {
   console.log(false);
 }
 
-
 //  additive identity  0
-//  multiplicative identity 1 
+//  multiplicative identity 1
 
+//
 
-// 
-
-
-// Check two given numbers and return true 
+// Check two given numbers and return true
 // if one of the numbers is 50 or if their sum is 50.
-
-
 // Check from the given integer, whether it is positive or negative.
-
-//   mod return remainder 
+//   mod return remainder
 // Check whether a given number is even or odd.
 // Check whether a given positive number is a multiple of 3.
+// Check from the given two numbers, whether the first number
+// is "greater", "lesser" or "equal" to the second number.
+
+// Check from the three sides of the triangle.
+//  use conditions to determine and display weather the triangle
+//  is "Equilateral" (all sides are equal),
+//  "Isosceles" (two sides are equal), or "Scalene"
+// (no sides are equal)
+// (side1+side2)>side3) 
+let x, y, z;
+
+x = 35;
+y = 35;
+z = 350;
+
+// if else if
 
 
-//  a = -15 * -1 => 15 
-
-
-if(a < 0){
- a = a * -1;
+if (x == y) {
+  console.log("Isosceles x and y are equal ");
+} else if (x == z) {
+  console.log("Isosceles x and z are equal ");
+} else if (y == z) {
+  console.log("Isosceles y and z are equal ");
 }
+
+
+if (x == y) {
+  console.log("Isosceles x and y are equal ");
+}
+
+if (x == z) {
+  console.log("Isosceles x and z are equal ");
+}
+
+if (y == z) {
+  console.log("Isosceles y and z are equal ");
+}
+
+// Check from the given month (1-12) if it's
+// "Winter" (December-February), "Spring" (March-May),
+//  "Summer" (June-August), or "Autumn" or "Fall" (September-November).
+
+//  a = -15 * -1 => 15
+
+// if (a > b) {
+// } else {
+// }
+
+// if (a < 0) {
+//   a = a * -1;
+// } else {
+//   console.log("abc");
+// }
