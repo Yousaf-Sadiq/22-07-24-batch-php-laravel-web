406d432ed2bd6050d0a7acc59605302df7e92e72
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-08-28 15:39:41 +0500

    calculator

---
+++ batch22-07-24/php/varaible.php
@@ -0,0 +1,23 @@
+<?php 
+
+$a=6;
+// concatination
+// echo "value of variable A is: ".$a;
+// string template 
+// echo "<br> value of variable A is: {$a}";
+http://localhost/BATCH_22_JULY_2024/varaible.php
+// Find the area of a rectangle where the length is 5 and the width is 8.
+// Find the area of a triangle where the base is 4 and the height is 3.
+// Find the area of a circle where the radius is 3.
+// Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius.
+
+if ($a <= 6) {
+ echo "<h1>value of variable A is: {$a}</h1>";
+}
+else{
+ echo "value of variable A  is greater then 6 : ".$a;
+}
+
+
+
+?>
